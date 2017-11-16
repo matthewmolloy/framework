62289469c0f11225d9b91356193124226c57ffe2
Matthew Molloy <contact@matthewmolloy.com.au>
2017-11-16 14:36:29 +0800

    Gulpfile overhaul featuring new build functionality

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 # New Project Framework
 
-This repository contains a lightweight starter framework for your new project using **NPM** and **Gulp**.
+This repository contains an easy to use, lightweight, front-end oriented framework for your new project using **NPM** and **Gulp**.
 
 Bundled as a **SASS** friendly environment, complete with **BrowserSync** for ease of development.
+
+
+## Features
+
+* JavaScript ready
+* SASS support
+* NPM support
+* Live browser preview
+* Code build for production
+* Automatic build file .zip compression
+* Command Prompt Feedback & Error Reporting
 
 
 ## How Does it Work?
@@ -32,9 +43,15 @@ This framework uses **Gulp** to both watch and compile the project. Run the foll
 $ gulp
 ~~~
 
-Running gulp will compile the contents of the src folder, minifying any found stylesheets and JavaScript, then output it as a bundled production-ready dist folder. 
+Running gulp will compile the contents of the src folder, and copy them to the dist folder.
 
 It will also launch **BrowserSync**, which will open a build of your project **(localhost:3000)** and watch for any changes. If a change is made, the project will automatically recompile and your browser page will refresh.
+
+~~~
+$ gulp build
+~~~
+
+Running gulp build will compile the contents of the **src** folder, minifying any found stylesheets and JavaScript, then output it as a bundled production-ready **build** folder. It will also compress the build folder as a .zip file.
 
 
 ## Further Documentation
@@ -43,8 +60,3 @@ It will also launch **BrowserSync**, which will open a build of your project **(
 * [Gulp](http://gulpjs.com/) - Automation toolkit.
 * [NPM](https://www.npmjs.com/) - Package manager.
 * [SASS](http://sass-lang.com/) - CSS extension language.
-
-
-## Special Thanks
-
-A special thank you must go to my former colleague [Leigh Gittoes](https://github.com/leigit), whose [Gulp-Sass-Starter](https://github.com/leigit/Gulp-Sass-Starter) formed the base of my own starter framework.
--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,36 @@
 // ----- Gulp File ----- //
 
-// Requirements //
+/*
 
-var browserSync = require('browser-sync');
-var gulp        = require('gulp');
-var minifycss   = require('gulp-minify-css');
-var plumber     = require('gulp-plumber');
-var rename      = require('gulp-rename');
-var sass        = require('gulp-sass');
-var uglify      = require('gulp-uglify');
-const babel     = require('gulp-babel');
+// Key Functions //
+
+gulp:               launches localhost:3000 and watches all files for changes
+gulp build:         compiles, minifies and compresses code for deployment
+
+*/
+
+// Dependencies //
+
+const browserSync   = require('browser-sync');
+const log           = require('fancy-log');
+const gulp          = require('gulp');
+const babel         = require('gulp-babel');
+const build         = require('gulp-build');
+const changed       = require('gulp-changed');
+const clean         = require('gulp-clean');
+const foreach       = require('gulp-foreach');
+const minifycss     = require('gulp-minify-css');
+const plumber       = require('gulp-plumber');
+const rename        = require('gulp-rename');
+const sass          = require('gulp-sass');
+const uglify        = require('gulp-uglify');
+const zip           = require('gulp-vinyl-zip');
+const path          = require('path');
 
 // Browser-Sync //
 
 gulp.task('browser-sync', function() {
+  log('Starting Browser-Sync');
   browserSync({
     server: {
       baseDir: "./dist/"
@@ -21,76 +38,189 @@ gulp.task('browser-sync', function() {
   });
 });
 
-gulp.task('bs-reload', function () {
+gulp.task('bs-reload', function() {
   browserSync.reload();
+  log('Browser Reloaded');
 });
 
 // Gulp Watch //
 
-gulp.task('watch', ['browser-sync'], function(){
-    gulp.watch("src/**/*.html", ['html']);
-    gulp.watch("src/**/*.js", ['scripts']);
-    gulp.watch("src/**/*.scss", ['styles']);
-    gulp.watch("src/**/*", ['images']);
-    gulp.watch("src/**/*.{ttfc,woff,woff2,eof,svg}", ['fonts']);
-    gulp.watch("src/**/*.html", ['bs-reload']);
+gulp.task('watch', ['browser-sync'], function() {
+  log('Watching Files for Change');
+  gulp.watch("src/**/*.html", ['html']);
+  gulp.watch("src/**/*.js", ['scripts']);
+  gulp.watch("src/**/*.scss", ['styles']);
+  gulp.watch("src/**/*", ['media']);
+  gulp.watch("src/**/*.{ttfc,woff,woff2,eof,svg}", ['fonts']);
+  gulp.watch("src/**/*.html", ['bs-reload']);
 });
 
 // HTML Pipe //
 
-gulp.task('html', function(){
-  gulp.src('src/**/*.html')
-  .pipe(gulp.dest('./dist/'))
+gulp.task('html', function() {
+  log('Piping HTML Files to Dist');
+  gulp.src(['src/**/*.html', 'src/**/*.json'])
+    .pipe(changed('./dist/'))
+    .pipe(gulp.dest('./dist/'))
+    .pipe(browserSync.reload({ stream: true }))
 });
 
 // Script Pipe //
 
-gulp.task('scripts', function(){
-  return gulp.src('src/**/*.js')
-  .pipe(plumber({
-    errorHandler: function (error) {
-      console.log(error.message);
-      this.emit('end');
-    }
-  }))
-  .pipe(rename({suffix: '.min'}))
-  .pipe(babel({presets: ['es2015']}))
-  .pipe(uglify())
-  .pipe(gulp.dest('./dist/'))
-  .pipe(browserSync.reload({stream:true}))
+gulp.task('scripts', function() {
+  log('Piping JavaScript to Dist');
+  return gulp.src(['src/**/*.js', '!**/*.min.js'])
+    .pipe(plumber({
+      errorHandler: function(error) {
+        log(error.message);
+        this.emit('end');
+      }
+    }))
+    .pipe(changed('./dist/'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('./dist/'))
+    .pipe(browserSync.reload({ stream: true }))
+});
+
+// Vendor Script Pipe //
+
+gulp.task('vendor', function() {
+  log('Piping Vendor Files to Dist');
+  return gulp.src('src/**/*.min.js')
+    .pipe(plumber({
+      errorHandler: function(error) {
+        log(error.message);
+        this.emit('end');
+      }
+    }))
+    .pipe(changed('./dist/'))
+    .pipe(gulp.dest('./dist/'))
+    .pipe(browserSync.reload({ stream: true }))
 });
 
 // SCSS Compile + Pipe //
 
-gulp.task('styles', function(){
+gulp.task('styles', function() {
+  log('Piping Styles to Dist');
   gulp.src(['src/**/*.scss'])
-  .pipe(plumber({
-    errorHandler: function (error) {
-      console.log(error.message);
-      this.emit('end');
-    }
-  }))
-  .pipe(sass())
-  .pipe(rename({suffix: '.min'}))
-  .pipe(minifycss())
-  .pipe(gulp.dest('./dist/'))
-  .pipe(browserSync.reload({stream:true}))
+    .pipe(plumber({
+      errorHandler: function(error) {
+        log(error.message);
+        this.emit('end');
+      }
+    }))
+    .pipe(sass())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('./dist/'))
+    .pipe(browserSync.reload({ stream: true }))
 });
 
 // Fonts Pipe //
 
-gulp.task('fonts', function(){
+gulp.task('fonts', function() {
+  log('Piping Fonts to Dist');
   gulp.src('src/**/*.{ttf,woff,woff2,eof,svg,txt}')
-  .pipe(gulp.dest('./dist/'))
+    .pipe(changed('./dist/'))
+    .pipe(gulp.dest('./dist/'))
+    .pipe(browserSync.reload({ stream: true }))
 });
 
-// Images Pipe //
+// Images and Video Pipe //
 
-gulp.task('images', function(){
-  gulp.src('src/**/*.{png,ico,svg,jpg,jpeg,gif,tiff,raw}')
-  .pipe(gulp.dest('./dist/'))
+gulp.task('media', function() {
+  log('Piping Images and Video to Dist');
+  gulp.src('src/**/*.{png,ico,svg,jpg,jpeg,gif,tiff,raw,mp4,mpeg,mov,avi}')
+    .pipe(changed('./dist/'))
+    .pipe(gulp.dest('./dist/'))
+    .pipe(browserSync.reload({ stream: true }))
 });
 
 // Gulp Default //
 
-gulp.task('default', ['watch', 'html', 'scripts', 'styles', 'fonts', 'images']);
+gulp.task('default', ['watch', 'html', 'scripts', 'vendor', 'styles', 'fonts', 'media']);
+
+// Gulp Compile //
+gulp.task('clean', function(cb) {
+  log('Clearing build files');
+  return gulp.src(['build', 'compressed/'], { read: false })
+    .pipe(clean())
+    .on('end', function() {
+      log('Cleared existing build files')
+    })
+})
+
+gulp.task('compile', ['clean'], function(cb) {
+
+  log('Building Project for Deployment');
+
+  return Promise.all([
+    new Promise((resolve, reject) => {
+      log('Piping HTML Files to Build');
+      gulp.src(['src/**/*.html', 'src/**/*.json'])
+        .on('error', reject)
+        .pipe(gulp.dest('./build/'))
+        .on('end', resolve)
+    }),
+    new Promise((resolve, reject) => {
+      log('Minifying JavaScript');
+      gulp.src(['src/**/*.js', '!**/*.min.js'])
+        .on('error', reject)
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(babel({ presets: ['es2015'] }))
+        .pipe(uglify())
+        .pipe(gulp.dest('./build/'))
+        .on('end', resolve)
+    }),
+    new Promise((resolve, reject) => {
+      log('Piping Vendor Files to Build');
+      gulp.src('src/**/*.min.js')
+        .on('error', reject)
+        .pipe(gulp.dest('./build/'))
+        .on('end', resolve)
+    }),
+    new Promise((resolve, reject) => {
+      log('Minifying Styles');
+      gulp.src(['src/**/*.scss'])
+        .on('error', reject)
+        .pipe(sass())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(minifycss())
+        .pipe(gulp.dest('./build/'))
+        .on('end', resolve)
+    }),
+    new Promise((resolve, reject) => {
+      log('Piping Fonts to Build');
+      gulp.src('src/**/*.{ttf,woff,woff2,eof,svg,txt}')
+        .on('error', reject)
+        .pipe(gulp.dest('./build/'))
+        .on('end', resolve)
+    }),
+    new Promise((resolve, reject) => {
+      log('Piping Images & Videos to Build');
+      gulp.src('src/**/*.{png,ico,svg,jpg,jpeg,gif,tiff,raw,mp4}')
+        .on('error', reject)
+        .pipe(gulp.dest('./build/'))
+        .on('end', resolve)
+    })
+  ]).then(function() {
+    log('Built all files')
+  });
+
+  // End Compile //
+
+});
+
+// Build & Compress for Deployment //
+
+gulp.task('build', ['compile'], function() {
+  log('Compressing Files for Deployment');
+  return gulp.src(['build'])
+    .pipe(foreach(function(stream, file) {
+      var name = path.basename(file.path);
+      log('Compressing Website');
+      return gulp.src(['./build/' + name + '/**/*'])
+        .pipe(zip.dest('Archive' + '.zip'))
+    })).on('end', function() {
+      log('Finished compressing build file')
+    })
+});
